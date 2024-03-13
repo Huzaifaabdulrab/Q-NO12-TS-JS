d549ba0b9b7ba656c9f9a11b806991009f671318
Huzaifaabdulrab <149306467+Huzaifaabdulrab@users.noreply.github.com>
2024-03-14 00:21:23 +0500

    Add files via upload

---
+++ QNO12.js
@@ -0,0 +1,7 @@
+// 12. Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+var friends = ["Huzaifa", "Raza", "Ali", "Asad"];
+var massage1 = "You are nice person";
+console.log(friends[0], massage1);
+console.log(friends[1], massage1);
+console.log(friends[2], massage1);
+console.log(friends[3], massage1);
