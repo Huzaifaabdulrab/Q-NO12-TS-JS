1c17fe8c04587cc7b763a8c342ba159d2e888f1a
Huzaifaabdulrab <149306467+Huzaifaabdulrab@users.noreply.github.com>
2024-03-14 05:13:33 +0500

    Delete app.ts

--- app.ts
+++
@@ -1,7 +0,0 @@
-// 12. Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
-let names : string[] = ["Huzaifa", "Asad", "Aryan","Muzamil"]
-let message : string= " You are nice person";
-console.log(names[0] + " " + message)
-console.log(names[1] + " " + message)
-console.log(names[2] + " " + message)
-console.log(names[3] + " " + message)
